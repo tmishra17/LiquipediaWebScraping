704f8abd8ced5017d9fa6f1fca20f387530107e1
tmishra17 <jetmi@live.com>
2024-03-19 23:26:00 -0700

    Modified Apriori Code

--- apriori.py
+++ apriori.py
@@ -1,34 +1,26 @@
 import numpy as np 
 import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori, association_rules 
 
 data = pd.read_csv("liqwinners.csv")
 
-print(f"Data Head: {data.head()}")
+transactions = []
+for year, characters in data.groupby('year'):
+    transactions.append(list(characters['character']))
 
-print(f"Data Columns: {data.columns}")
+print(len(data['year']))
 
-# split data by year
+# preprocessing
+te = TransactionEncoder()
+te_arr = te.fit(transactions).transform(transactions)
+df = pd.DataFrame(te_arr, columns=te.columns_)
+# Apriori
+freqeunt_itemsets = apriori(df, min_support=0.025, use_colnames=True)
+rules = association_rules(freqeunt_itemsets, metric="confidence", min_threshold=0.7)
 
-print(f"Year: {data['tournament_year']}")
 
-# basket_2004 = 
-# basket_2005 = 
-# basket_2006 = 
-# basket_2007 = 
-# basket_2008 = 
-# basket_2009 = 
-# basket_2010 = 
-# basket_2011 = 
-# basket_2012 = 
-# basket_2014 = 
-# basket_2015 = 
-# basket_2016 = 
-# basket_2017 = 
-# basket_2018 = 
-# basket_2019 = 
-# basket_2020 = 
-# basket_2021 = 
-# basket_2022 = 
-# basket_2023 = 
 
+rules.to_csv("meleeTournamentRules.csv")
+
+print(rules) 
